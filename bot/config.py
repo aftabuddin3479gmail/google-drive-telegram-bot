0790f0d84e25d7ee070ae62101b32d89520e94e1
aftabuddin3479gmail <75784579+aftabuddin3479gmail@users.noreply.github.com>
2020-12-12 12:21:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -17,7 +17,7 @@ class BotCommands:
   Delete = ['delete', 'del']
   EmptyTrash = ['emptyTrash']
   Ytdl = ['ytdl']
-
+  
 class Messages:
     START_MSG = "**Hi there {}.**\n__I'm Google Drive Uploader Bot.You can use me to upload any file / video to Google Drive from direct link or Telegram Files.__\n__You can know more from /help.__"
 
