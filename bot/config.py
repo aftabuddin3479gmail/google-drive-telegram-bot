83cfcac239fae1af01c4bcd432aa7c57e61d1d1b
aftabuddin3479gmail <75784579+aftabuddin3479gmail@users.noreply.github.com>
2020-12-16 00:02:38 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,7 @@ class config:
     API_HASH = "cbb279159c065dbb3ab1e3160b004518"
     DATABASE_URL = "postgres://dvchwqfkuuenpa:a892026422fbb220e3e64d31ab0b9a1807c483a00826d5d692b10e52b9b55c7f@ec2-18-233-207-22.compute-1.amazonaws.com:5432/da6dt8b7ukqei5"
     SUDO_USERS = "1366454553" # Sepearted by space.
-    SUPPORT_CHAT_LINK = "@chatsupport99"
+    SUPPORT_CHAT_LINK = "https://t.me/chatsupport99"
     DOWNLOAD_DIRECTORY = "./downloads/"
 
 
